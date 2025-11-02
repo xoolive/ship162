36459dda546935cfc8f81286319aaec9f38e3807
Abraham Cheung <58929011+abc8747@users.noreply.github.com>
2025-11-03 05:58:05 +0800

    refactor: reuse `iq` abstraction in `rtlsdr`
    - the public-facing `RtlSdrReceiver` and `AsyncRtlSdrReceiver` are now thin
      wrappers around `IqSource` and `AsyncIqSource`.
    - improved error handling by propagating I/O errors through the `Iterator`
      and `Stream` as `Result` items
    - bumped `rtl-sdr-rs` to 0.2.0 to support R828D tuner
    - test: fix false positive in `test_message_serialisation`
    
    Can confirm it works with rtl-sdr blog v4 (r828d) with this patch applied:
    https://github.com/ccostes/rtl-sdr-rs/pull/24
    
    ```sh
    cargo run --release --example rtlsdr
    cargo run --release --example rtlsdr_async
    ```

--- crates/rs162/examples/rtlsdr.rs
+++ crates/rs162/examples/rtlsdr.rs
@@ -1,19 +1,28 @@
 use rs162::{decode::mmsi::MmsiInfo, sources::rtlsdr::RtlSdrReceiver};
+use rtl_sdr_rs::error::Result;
 use serde_json::json;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<()> {
     let receiver = RtlSdrReceiver::new()?;
 
-    for msg in receiver {
-        if let Some(ais_msg) = msg.decode() {
-            let output = json!({
-                "signal_level": msg.signal_level,
-                "timestamp": msg.timestamp,
-                "channel": msg.channel,
-                "message": ais_msg,
-                "mmsi_info": MmsiInfo::from_message(&ais_msg).ok()
-            });
-            println!("{}", serde_json::to_string(&output).unwrap());
+    for msg_res in receiver {
+        match msg_res {
+            Ok(msg) => {
+                if let Some(ais_msg) = msg.decode() {
+                    let output = json!({
+                        "signal_level": msg.signal_level,
+                        "timestamp": msg.timestamp,
+                        "channel": msg.channel,
+                        "message": ais_msg,
+                        "mmsi_info": MmsiInfo::from_message(&ais_msg).ok()
+                    });
+                    println!("{}", serde_json::to_string(&output).unwrap());
+                }
+            }
+            Err(e) => {
+                eprintln!("I/O error reading samples: {e}");
+                continue;
+            }
         }
     }
 
